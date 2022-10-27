6d911c4b9f7fd5fb0cf026969075b126998746d1
sultan288 <sultan.ist28@gmail.com>
2022-10-28 01:21:57 +0600

    update28oct

---
+++ CSharpPractice/IntroToCShar/ReadLine strint to int conversion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IntroToCShar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter firstname: ");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Enter lastname: ");
+            string lastname = Console.ReadLine();
+
+            //Using Place holder syntax
+            //Console.WriteLine("Hello {0}", username);
+
+            Console.WriteLine("My full name: {0} {1}", firstname, lastname);
+
+            //Using Concatenation
+            //Console.WriteLine("My full name: " + firstname +" " + lastname);
+
+            Console.WriteLine("Enter first no: ");
+            int firstno = Int32.Parse(Console.ReadLine()); // Using Int32.Parse
+            Console.WriteLine("Enter last no: ");
+            int lastno = Convert.ToInt32(Console.ReadLine());  // Using Convert.ToInt32
+            int result = firstno + lastno;
+
+            Console.WriteLine("My Result is {0} + {1} = {2}", firstno, lastno, result);
+        } 
+       
+    }
+}
